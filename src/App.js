c54a0dba68ebf6f8d34f8c9c9501e372fc53ece2
shyamkumaryadav <shyamkumaryadav2003@gmail.com>
2020-10-26 14:28:47 +0530

    Failed to compile :rocket:

--- src/App.js
+++ src/App.js
@@ -1,21 +1,103 @@
 import React from 'react';
-import Hadder from "./nav/Hadder";
-import Cards from "./components/Cards";
-import TestRoute from './components/TestRoute';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { withStyles } from '@material-ui/core/styles';
+import { purple } from '@material-ui/core/colors';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import AppBarU from "./layout/appbar";
 
 
 function App() {
+  const [dark, setdark] = React.useState(false)
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: dark ? 'dark' : 'light',
+        },
+      }),
+    [dark],
+  );
+
+  const IOSSwitch = withStyles((theme) => ({
+    root: {
+      width: 42,
+      height: 26,
+      padding: 0,
+      margin: theme.spacing(1),
+    },
+    switchBase: {
+      padding: 1,
+      '&$checked': {
+        transform: 'translateX(16px)',
+        color: theme.palette.common.white,
+        '& + $track': {
+          backgroundColor: '#52d869',
+          opacity: 1,
+          border: 'none',
+        },
+      },
+      '&$focusVisible $thumb': {
+        color: '#52d869',
+        border: '6px solid #fff',
+      },
+    },
+    thumb: {
+      width: 24,
+      height: 24,
+    },
+    track: {
+      borderRadius: 26 / 2,
+      border: `1px solid ${theme.palette.grey[400]}`,
+      backgroundColor: theme.palette.grey[50],
+      opacity: 1,
+      transition: theme.transitions.create(['background-color', 'border']),
+    },
+    checked: {},
+    focusVisible: {},
+  }))(({ classes, ...props }) => {
+    return (
+      <Switch
+        focusVisibleClassName={classes.focusVisible}
+        disableRipple
+        classes={{
+          root: classes.root,
+          switchBase: classes.switchBase,
+          thumb: classes.thumb,
+          track: classes.track,
+          checked: classes.checked,
+        }}
+        {...props}
+      />
+    );
+  });
+  
+
   return (
-    <Router>
-      <React.Fragment>
-        <header className="App-header">
-          <Hadder />
-          <Route path='/' exact component={Cards} />
-          <Route path='/:name' exact component={TestRoute} />
-        </header>
-      </React.Fragment>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <AppBarU></AppBarU>
+      <CssBaseline/>
+      <FormControlLabel
+        control={<IOSSwitch checked={dark} onChange={() => setdark(!dark)} name="checkedB" />}
+        label="Dark"
+      />
+      <Typography component="div">
+        <Grid component="label" container alignItems="center" spacing={1}>
+          <Grid item>Light</Grid>
+          <Grid item>
+            <IOSSwitch checked={dark} onChange={() => setdark(!dark)} name="dark" />
+          </Grid>
+          <Grid item>Dark</Grid>
+        </Grid>
+      </Typography>
+      <h1>Hello Theme</h1>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
+import theme from './theme';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <App />,
+  document.querySelector('#root'),
 );
